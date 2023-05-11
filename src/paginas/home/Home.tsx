24d48b791bec55bdb93b5d1f7435e3ad979efb2d
AzvalSG <giovanni0205.guimaraes@gmail.com>
2023-05-11 11:27:43 -0300

    Atualização Home

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
-
 function Home() {
     return (
-        <div>Home</div>
+
+
+        <div>
+            <ul>
+                <li><a href="Index.html">Home</a></li>
+                <li><a href="Contato.html">Contato</a></li>
+            </ul>
+
+
+
+            <h1>Sobre mim</h1>
+            <p>Olá, meu nome é Giovanni e eu sou um estudante de programação. Eu estou aprendendo HTML, CSS e JavaScript
+                para criar sites e aplicativos da web.</p>
+            <p>Eu gosto de jogar videogame, ler livros e ouvir música. Eu também sou um grande fã de tecnologia e estou
+                sempre procurando aprender coisas novas.</p>
+            <p>Este site foi criado como um projeto de prática para me ajudar a desenvolver minhas habilidades de HTML.
+            </p>
+                <h2>Meu video favorito</h2>
+
+                <p><iframe width="800" height="600" src="https://www.youtube.com/embed/bS4Q-WWyl3Q"
+                    title="&quot;Toss A Coin To Your Witcher&quot; METAL COVER" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe></p>
+                <footer>
+                    <p>
+                        Todos os direitos reservados: Giovanni Santos &copy
+                    </p>
+
+                </footer>
+        </div>
     )
 }
 
