4822973318ffc63dbfda284c438802f29c012421
AzvalSG <giovanni0205.guimaraes@gmail.com>
2023-05-22 10:07:42 -0300

    Inserção da lista de postagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css'
-import Footer from './componentes/estaticos/footer/Footer'
-import Navbar from './componentes/estaticos/navbar/Navbar'
-import Home from './paginas/home/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './paginas/login/Login';
+import Footer from './componentes/estaticos/footer/Footer';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import ListaPostagem from './componentes/postagens/listapostagem/ListaPostagem';
+import ListaTema from './componentes/temas/listatema/Listatema';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 function App() {
   return (
     <BrowserRouter>
@@ -16,6 +16,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          <Route path="/temas" element={<ListaTema />} />
+          <Route path="/posts" element={<ListaPostagem />} />
         </Routes>
       </div>
       <Footer />
